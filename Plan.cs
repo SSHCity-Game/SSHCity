6ebe96024eb3496032877e48d267be3a0f2e0b7f
Drevet Florian <florian.drevet@epita.fr>
2020-03-09 16:00:51 +0100

    eau aleatoire sur la map 2.0

--- Plan.cs
+++ Plan.cs
@@ -7,26 +7,145 @@ public class Plan : Node2D
     private Random rand;
     private TileMap _tileMap1;
     private TileMap _tileMap2;
-    private int block_eau = 15;
-    
-    
+    private int max_x = 31;
+    private int min_x = -16;
+    private int max_y = 19;
+    private int min_y = -29;
+    private int max_flaque_eau = 4;
+    private int min_flaque_eau = 2;
+    private int max_block_flaque_eau = 10;
+    private int min_block_flaque_eau = 5;
+
+
     public override void _Ready()
     {
         rand = new Random();
+        int nbr_flaque_eau = rand.Next(min_flaque_eau, max_flaque_eau+1);
         _tileMap1 = (TileMap) GetNode("Navigation2D/TileMap1");
         _tileMap2 = (TileMap) GetNode("Navigation2D/TileMap2");
-        for (int i = 0; i < block_eau; i++)
+        for (int i = 0; i < nbr_flaque_eau; i++)
         {
-            int rand_x = rand.Next(-16, 31);
-            int rand_y = rand.Next(-29, 19);
+            int rand_x = rand.Next(min_x, max_x+1);
+            int rand_y = rand.Next(min_y, max_y+1);
             int indexe = _tileMap1.GetCell(rand_x, rand_y);
-            if (indexe == -1)
+            if (indexe == 2)
             {
-                i--;
-            }
-            else
-            {
-                _tileMap1.SetCell(rand_x, rand_y, 2, false, false, false);
+                int nbr_block_eau = rand.Next(min_block_flaque_eau, max_block_flaque_eau+1);
+                int j = 0;
+                while (j < nbr_block_eau)
+                {
+                    int deplacement = rand.Next(0, 4);
+
+                    switch (deplacement)
+                    {
+                        case 0:
+                        {
+                            if (_tileMap1.GetCell(rand_x+1, rand_y) == 0)
+                            {
+                                rand_x++;
+                                _tileMap1.SetCell(rand_x, rand_y, 2);
+                                j++;
+                                if (_tileMap1.GetCell(rand_x+1, rand_y) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x+1, rand_y, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x, rand_y+1) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x, rand_y+1, 2);
+                                    j++;
+                                }
+
+                                if (_tileMap1.GetCell(rand_x, rand_y - 1) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x, rand_y - 1, 2);
+                                    j++;
+                                }
+                            }
+                            break;
+                        }
+
+                        case 1:
+                        {
+                            if (_tileMap1.GetCell(rand_x-1, rand_y) == 0)
+                            {
+                                rand_x--;
+                                _tileMap1.SetCell(rand_x-1, rand_y, 2);
+                                j++;
+                                if (_tileMap1.GetCell(rand_x-1, rand_y) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x-1, rand_y, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x, rand_y+1) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x, rand_y+1, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x, rand_y-1) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x, rand_y-1, 2);
+                                    j++;
+                                }
+                            }
+                            break;
+                        }
+
+                        case 2:
+                        {
+                            if (_tileMap1.GetCell(rand_x, rand_y+1) == 0)
+                            {
+                                rand_y++;
+                                _tileMap1.SetCell(rand_x, rand_y, 2);
+                                j++;
+                                if (_tileMap1.GetCell(rand_x+1, rand_y) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x+1, rand_y, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x-1, rand_y) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x-1, rand_y, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x, rand_y+1) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x, rand_y+1, 2);
+                                    j++;
+                                }
+                                
+                            }
+                            break;
+                        }
+                        case 3:
+                        {
+                            if (_tileMap1.GetCell(rand_x, rand_y-1) == 0)
+                            {
+                                rand_y--;
+                                _tileMap1.SetCell(rand_x, rand_y, 2);
+                                j++;
+                                if (_tileMap1.GetCell(rand_x+1, rand_y) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x+1, rand_y, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x-1, rand_y) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x-1, rand_y, 2);
+                                    j++;
+                                }
+                                if (_tileMap1.GetCell(rand_x, rand_y-1) == 0)
+                                {
+                                    _tileMap1.SetCell(rand_x, rand_y-1, 2);
+                                    j++;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    
+    
+                }
             }
         }
     }
