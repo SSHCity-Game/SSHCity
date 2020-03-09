90f1402bf57c52ca7d21a42048b3120968978997
Drevet Florian <florian.drevet@epita.fr>
2020-03-09 15:20:34 +0100

    eau aleatoire sur la map

--- Plan.cs
+++ Plan.cs
@@ -7,6 +7,7 @@ public class Plan : Node2D
     private Random rand;
     private TileMap _tileMap1;
     private TileMap _tileMap2;
+    private int block_eau = 15;
     
     
     public override void _Ready()
@@ -14,7 +15,20 @@ public class Plan : Node2D
         rand = new Random();
         _tileMap1 = (TileMap) GetNode("Navigation2D/TileMap1");
         _tileMap2 = (TileMap) GetNode("Navigation2D/TileMap2");
-        _tileMap1.SetCell( 5, 5, 2, false, false, false);
+        for (int i = 0; i < block_eau; i++)
+        {
+            int rand_x = rand.Next(-16, 31);
+            int rand_y = rand.Next(-29, 19);
+            int indexe = _tileMap1.GetCell(rand_x, rand_y);
+            if (indexe == -1)
+            {
+                i--;
+            }
+            else
+            {
+                _tileMap1.SetCell(rand_x, rand_y, 2, false, false, false);
+            }
+        }
     }
     
 }
