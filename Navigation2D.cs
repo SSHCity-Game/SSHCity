29597337db988b87ee6bf1f904e257370ce1ce53
Annabelle Chevreau <annabelle.chevreau@epita.fr>
2020-03-09 15:56:02 +0100

    ajout incident

--- Navigation2D.cs
+++ Navigation2D.cs
@@ -1,19 +1,12 @@
 using Godot;
 using System;
 
-public class Building : Node
+public class Navigation2D : Godot.Navigation2D
 {
-	private int size;
-	private int level;
-	private int price;
-	private int time_to_build;
-	private int money = 100000000;
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-	public Building(int size, int level)
-	{
-		this.size = size;
-		this.level = level;
-	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
--- TileMap2.cs
+++ TileMap2.cs
@@ -3,13 +3,13 @@ using System;
 
 public class TileMap2 : TileMap
 {
-    // Declare member variables here. Examples:
+	// Declare member variables here. Examples:
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
