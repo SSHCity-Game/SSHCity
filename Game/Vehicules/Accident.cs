3e3b97ca41fe36516da24bb47553c55104bc0a17
Drevet Florian <florian.drevet@epita.fr>
2020-05-19 14:23:11 +0200

    Commentaire Accident.cs

--- Game/Vehicules/Accident.cs
+++ Game/Vehicules/Accident.cs
@@ -4,7 +4,6 @@ using System;
 public class Accident : Area2D
 {
     private bool _mouseIn = false;
-    private PlanInitial _planInitial;
     private Sprite _sprite;
     private const string _strSprite = "Sprite";
 
@@ -16,37 +15,55 @@ public class Accident : Area2D
         Connect("area_exited", this, nameof(AreaExited));
     }
 
+    /// <summary>
+    /// Lorsqu'une Area2D sort, supprime cette area2D. Permet lors de la resolution de l'accident d'arreter les bouchons 
+    /// </summary>
+    /// <param name="area2D">Area2D venant de sortir de cette area2D</param>
     public void AreaExited(Area2D area2D)
     {
-        if (area2D.CollisionMask == 3)
+        if (area2D.CollisionMask == 3) // Verifie que l'area venant de sortir est une area2D de CollsionMask accident, afin de ne pas supprimer les collsionMask de vehicules. 
         {
             QueueFree();
         }
     }
 
+    /// <summary>
+    /// Lorsque la souris passe sur la zone de l'accident
+    /// </summary>
     public void MouseEntered()
     {
         _mouseIn = true;
     }
+    
+    /// <summary>
+    /// Lorsque la souris sors de la zone de l'accident 
+    /// </summary>
     public void MouseExited()
     {
         _mouseIn = false;
     }
 
+    /// <summary>
+    /// Fonction de Godot recuperant les actions faites par le joueur 
+    /// </summary>
+    /// <param name="OneAction">Recupere l'action faite par l'utilisateur</param>
     public override void _Input(InputEvent OneAction)
     {
         base._Input(OneAction);
-        if (OneAction.IsActionPressed("ClickG") && _mouseIn )
+        
+        //TODO changer la condition suivante pour résoudre l'accident
+        if (OneAction.IsActionPressed("ClickG") && _mouseIn ) //Lorsqu'il fait clic G et que la souris est sur la zone de l'accident
         {
-            Vector2 position = _planInitial.TileMap2.WorldToMap(GetGlobalMousePosition());
-            _planInitial.SetBlock(_planInitial.TileMap3, (int)position.x, (int)position.y, -1);
-            this.QueueFree();
+            this.QueueFree(); //supprime la zone d'accident
         }
     }
 
-    public void Init(PlanInitial planInitial, bool visi)
+    /// <summary>
+    /// Constructeur de la zone d'accident
+    /// </summary>
+    /// <param name="visi">Indique si l'on voit l'image de l'accident ou s'il s'gait juste d'une zone pour faire les bouchons</param>
+    public void Init(bool visi)
     {
-        _planInitial = planInitial;
         _sprite = (Sprite) GetNode(_strSprite);
         _sprite.Visible = visi;
     }
